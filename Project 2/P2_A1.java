0d07228de5b3a914ed3d3e79213e2e90bb1a425a
kingsman142 <kingsman142@yahoo.com>
2016-02-08 14:30:19 -0500

    added project 2

---
+++ Project 2/P2_A1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class P2_A1{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter your name: ");
+		String name = sc.next();
+
+		System.out.printf("Hello %s!\n", name);
+		System.out.println("My name is James. You are about to play the most exhilerating dice game of your life.  Welcomem to Rockymon!");
+		System.out.println("The aim of the game is to roll two die to either get a 5 or a 10!");
+	}
+}
